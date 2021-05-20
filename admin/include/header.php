20011d08e6833ffe97fa29cb2213e9e7b68a771c
Simran Dingani <simran.dingani.2000@gmail.com>
2021-05-20 17:59:04 +0530

    Initial commit

---
+++ admin/include/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+	<div class="navbar-header">
+			<a class="navbar-brand" href="#" style="font-size:24px">
+				Admin Panel	</a>		</div>
+
+<div class="collapse navbar-collapse">
+<ul class="nav navbar-nav navbar-right">
+<li class="dropdown ">
+<a href="#">Settings</a></li>
+        <li><a href=".././logout.php">Logout</a></li>
+	</ul>
+	  </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+	</nav>
---
+++ footer.php
@@ -0,0 +1,73 @@
+<footer id="footer">
+			<!-- top footer -->
+			<div class="section">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">About Us</h3>
+								<ul class="footer-links">
+									<li><a href="#"><i class="fa fa-map-marker"></i>Mumbai ,India</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>+91-3485127896</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>simran.dingani@gmail.com</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-md-6 text-center" style="margin-top:80px;">
+							<ul class="footer-payments">
+								<li><a href="#"><i class="fa fa-cc-visa"></i></a></li>
+								<li><a href="#"><i class="fa fa-credit-card"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-paypal"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-mastercard"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-discover"></i></a></li>
+								<li><a href="#"><i class="fa fa-cc-amex"></i></a></li>
+							</ul>
+							<span class="copyright">
+								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+								Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This website is made <!-- with <i class="fa fa-heart-o" aria-hidden="true"></i> --> by <a href="#" target="_blank">
+									<br>Students of Third Year Computer Science</a>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+							</span>
+						</div>
+
+						<div class="col-md-3 col-xs-6">
+							<div class="footer">
+								<h3 class="footer-title">Categories</h3>
+								<ul class="footer-links">
+									<li><a href="#">Mobiles</a></li>
+									<li><a href="#">Men</a></li>
+									<li><a href="#">Women</a></li>
+									<li><a href="#">Kids</a></li>
+									<li><a href="#">Accessories</a></li>
+								</ul>
+							</div>
+						</div>
+
+						<div class="clearfix visible-xs"></div>
+
+						
+					</div>
+					<!-- /row -->
+				</div>
+				<!-- /container -->
+			</div>
+			<!-- /top footer -->
+                
+
+			<!-- bottom footer -->
+			
+			<!-- /bottom footer -->
+		</footer>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/actions.js"></script>
+		<script src="js/sweetalert.min"></script>
+		<script src="js/jquery.payform.min.js" charset="utf-8"></script>
+    	<script src="js/script.js"></script>
+	
